804796dfbf634eebdd3eabc508af05f25ca14d72
Kai <thiskaiguy@gmail.com>
2020-03-27 13:39:16 +0000

    Setup autoreloading and create dev scripts

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use {
     actix_web::{get, App, HttpServer, Responder},
+    listenfd::ListenFd,
     std::env,
 };
 
@@ -10,14 +11,26 @@ async fn index() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    let port = server_port();
+    let mut listenfd = ListenFd::from_env();
 
-    println!("Starting server on http://0.0.0.0:{}", port);
+    let mut server = HttpServer::new(move || App::new().service(index));
 
-    HttpServer::new(move || App::new().service(index))
-        .bind(("0.0.0.0", port))?
-        .run()
-        .await
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => {
+            let port = listener.local_addr()?.port();
+
+            println!("Restarting server on http://0.0.0.0:{}", port);
+            server.listen(listener)?
+        },
+        None => {
+            let port = server_port();
+
+            println!("Starting server on http://0.0.0.0:{}", port);
+            server.bind(("0.0.0.0", port))?
+        },
+    };
+
+    server.run().await
 }
 
 fn server_port() -> u16 {
